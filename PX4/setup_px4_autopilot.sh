d950ee8be6b3f17a72830aecbfdf036fbe7e2930
RanFR <ranfr.mail@gmail.com>
2025-04-01 18:18:47 +0800

    Add PX4 environment setup script

---
+++ PX4/setup_px4_autopilot.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SRC_DIR="$HOME/Codes/Simulator/PX4-Autopilot"
+BUILD_DIR="$SRC_DIR/build/px4_sitl_default"
+
+# Define a function to check and add paths
+add_to_env_var() {
+    local VAR_NAME=$1
+    local VAR_VALUE=$2
+    local VAR_CONTENT=$(eval echo \$$VAR_NAME)
+
+    if [[ ":$VAR_CONTENT:" != *":$VAR_VALUE:"* ]]; then
+        export $VAR_NAME="$VAR_CONTENT:$VAR_VALUE"
+        echo "Added to $VAR_NAME: $VAR_VALUE"
+    else
+        echo "$VAR_NAME already contains: $VAR_VALUE"
+    fi
+}
+
+# Use the function to add paths
+add_to_env_var "ROS_PACKAGE_PATH" "${SRC_DIR}"
+add_to_env_var "GAZEBO_PLUGIN_PATH" "${BUILD_DIR}/build_gazebo"
+add_to_env_var "GAZEBO_MODEL_PATH" "${SRC_DIR}/Tools/sitl_gazebo/models"
+add_to_env_var "LD_LIBRARY_PATH" "${BUILD_DIR}/build_gazebo"
--- README.md
+++ README.md
@@ -8,11 +8,21 @@
 
 ## Clang
 
-- .clang-format
+Google style, created by *clang-format-10*.
 
 ## Clash
 
 Clash config files.
+
+## PX4
+
+For [PX4-Autopilot](https://github.com/PX4/PX4-Autopilot) environment.
+
+Change *SRC_DIR* and *BUILD_DIR* to the path you use, and then execute the following command
+
+```bash
+. setup_px4_autopilot.sh
+```
 
 ## WezTerm
 
